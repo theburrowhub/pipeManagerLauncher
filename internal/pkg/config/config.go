d45683c6a4c90d992a5ab07eebbda8bee8d3dd99
Sergio Tejón <stejon@freepik.com>
2024-09-27 00:02:17 +0200

    build: Update gitSecret path in config files
    Change: The gitSecret path has been updated from an empty list to '/etc/pipeManager/git-credentials' in the config_example.yaml file.
    Change: The gitSecretPath key has been added to the webhook-listener values.yaml file, defining the path to the Git credentials directory.

--- internal/pkg/config/config.go
+++ internal/pkg/config/config.go
@@ -51,13 +51,13 @@ type WebhookConfig struct {
 // LauncherStruct defines the launcher configuration.
 // It captures the image name, pull policy, tag, namespace, job name prefix, and timeout.
 type LauncherStruct struct {
-	ImageName     string   `yaml:"imageName"`     // ImageName is the name of the Docker image to be used
-	PullPolicy    string   `yaml:"pullPolicy"`    // PullPolicy is the policy to use when pulling the image
-	Tag           string   `yaml:"tag"`           // Tag is the tag of the Docker image to be used
-	Namespace     string   `yaml:"namespace"`     // Namespace is the Kubernetes namespace to deploy the job
-	JobNamePrefix string   `yaml:"jobNamePrefix"` // JobNamePrefix is the prefix to use for the job name
-	Timeout       int64    `yaml:"timeout"`       // Timeout is the maximum time in seconds to wait for the job to complete
-	GitSecret     []string `yaml:"gitSecret"`     // GitSecret is the list of names of secrets to use for the Git credentials
+	ImageName     string `yaml:"imageName"`     // ImageName is the name of the Docker image to be used
+	PullPolicy    string `yaml:"pullPolicy"`    // PullPolicy is the policy to use when pulling the image
+	Tag           string `yaml:"tag"`           // Tag is the tag of the Docker image to be used
+	Namespace     string `yaml:"namespace"`     // Namespace is the Kubernetes namespace to deploy the job
+	JobNamePrefix string `yaml:"jobNamePrefix"` // JobNamePrefix is the prefix to use for the job name
+	Timeout       int64  `yaml:"timeout"`       // Timeout is the maximum time in seconds to wait for the job to complete
+	GitSecretPath string `yaml:"gitSecretPath"` // GitSecretPath is the path to the directory containing the Git credentials
 }
 
 // LauncherConfig defines the launcher configuration.
