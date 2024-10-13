d827b1265ea6d9f13a504963f83f1552d5db62b2
Sergio Tejón <stejon@freepik.com>
2024-10-13 12:24:14 +0200

    refactor(cmd/run.go): set config file mandatory in run command

--- internal/app/launcher/cmd/run.go
+++ internal/app/launcher/cmd/run.go
@@ -1,7 +1,12 @@
 package cmd
 
 import (
+	"errors"
+	"os"
+
 	"github.com/spf13/cobra"
+
+	"github.com/sergiotejon/pipeManager/internal/pkg/logging"
 )
 
 // runCmd represents the run command
@@ -9,9 +14,23 @@ var runCmd = &cobra.Command{
 	Use:   "run",
 	Short: "Run the application",
 	Run: func(cmd *cobra.Command, args []string) {
-		// Setup the application
+
+		if err := validateCacheUploadFlags(); err != nil {
+			logging.Logger.Error("Invalid flags", "error", err)
+			os.Exit(1)
+		}
+
+		// Set up the application
 		setup()
 		// Run the main application
 		app()
 	},
+}
+
+// validateRunFlags checks if the provided flags are valid
+func validateRunFlags() error {
+	if configFile == "" {
+		return errors.New("config file is required")
+	}
+	return nil
 }
