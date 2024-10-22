b2614901bb8c57b35566c5894c147ac83f3cf2c4
Sergio Tejón <sergio.tejon@gmail.com>
2024-10-22 20:03:37 +0200

    refactor(cmd/pipeline-controller/main.go): Correct package import reference in main function

--- cmd/pipeline-controller/main.go
+++ cmd/pipeline-controller/main.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/sergiotejon/pipeManager/internal/app/pipieline-controller/cmd"
+import "github.com/sergiotejon/pipeManager/internal/app/pipeline-controller/cmd"
 
 // main is the entrypoint for the application
 // It sets up the root command and executes the application
