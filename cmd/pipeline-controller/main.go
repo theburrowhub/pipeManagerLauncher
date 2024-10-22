b6756e5f38dfa942db75d1a76417b1929a1ee147
Sergio Tejón <sergio.tejon@gmail.com>
2024-10-22 19:46:31 +0200

    refactor(cmd/pipeline-controller/main.go): Initial implementation of new pipeline controller

---
+++ cmd/pipeline-controller/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/sergiotejon/pipeManager/internal/app/pipieline-controller/cmd"
+
+// main is the entrypoint for the application
+// It sets up the root command and executes the application
+func main() {
+	cmd.Execute()
+}
