44bf4ff559ec2318b3be837aedeb86bd8a0e195d
Sergio Tejón <sergio.tejon@gmail.com>
2024-10-24 20:22:40 +0200

    feat(internal/pkg/k8s): Create new file for Kubernetes package interaction
    - Initialize empty file to facilitate future development of Kubernetes integration within the application

---
+++ internal/pkg/k8s/kubernetes.go
@@ -0,0 +1,40 @@
+package k8s
+
+import (
+	"os"
+	"path/filepath"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+
+	"github.com/sergiotejon/pipeManager/internal/pkg/logging"
+)
+
+// GetKubernetesClient returns a Kubernetes clientset configured for either in-cluster or local access
+func GetKubernetesClient() (*kubernetes.Clientset, error) {
+	// Try to get the in-cluster config
+	cfg, err := rest.InClusterConfig()
+	if err != nil {
+		logging.Logger.Warn("Failed to get in-cluster config, trying local config", "error", err)
+		// Fallback to local kubeconfig
+		var configPath string
+		if os.Getenv("KUBECONFIG") != "" { // Get the kubeconfig path from the KUBECONFIG environment variable
+			configPath = os.Getenv("KUBECONFIG")
+		} else { // If not set, use the default path ~/.kube/config
+			configPath = filepath.Join(os.Getenv("HOME"), ".kube", "config")
+		}
+		cfg, err = clientcmd.BuildConfigFromFlags("", configPath)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// Create a clientset for interacting with the Kubernetes API
+	client, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
