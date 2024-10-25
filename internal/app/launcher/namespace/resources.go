7819b55c25862ceb515dac35a0307230192fbf75
Sergio Tejón <sergio.tejon@gmail.com>
2024-10-25 23:16:09 +0200

    feat(internal/app/launcher/namespace/resources.go): Implement k8s client-go integration and add helper functions for namespace management
    - Update import statements to include "k8s.io/client-go/kubernetes"
    - Add function "checkIfResourceNamespaceExists" to check if a namespace with the given name exists
    - Add function "createResourceNamespace" to create a namespace with the given name and labels
    - Add function "updateResourceNamespaceLabels" to update the labels of a namespace with the given name if they are different
    - Add function "createOrUpdateServiceAccount" to create or update a service account with the given name and namespace

--- internal/app/launcher/namespace/resources.go
+++ internal/app/launcher/namespace/resources.go
@@ -3,10 +3,12 @@ package namespace
 import (
 	"context"
 	"fmt"
-	"k8s.io/client-go/kubernetes"
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+
+	"github.com/sergiotejon/pipeManager/internal/pkg/config"
 )
 
 // checkIfResourceNamespaceExists checks if a namespace with the given name exists
@@ -24,6 +26,7 @@ func createResourceNamespace(client *kubernetes.Clientset, name string, labels m
 	// TODO:
 	// Add specific labels to the namespace for the application (see webhook-listener for an example)
 	// Send that labels to common packaged for reuse
+	// Like helm charts with its labels
 
 	ns := &corev1.Namespace{
 		ObjectMeta: metav1.ObjectMeta{
@@ -58,6 +61,39 @@ func updateResourceNamespaceLabels(client *kubernetes.Clientset, name string, la
 	_, err = client.CoreV1().Namespaces().Update(context.TODO(), ns, metav1.UpdateOptions{})
 	if err != nil {
 		return fmt.Errorf("failed to update namespace labels: %w", err)
+	}
+
+	return nil
+}
+
+// createOrUpdateServiceAccount creates or updates a service account with the given name and namespace
+func createOrUpdateServiceAccount(client *kubernetes.Clientset, saName string, namespace string) error {
+	// Check if the service account already exists
+	sa, err := client.CoreV1().ServiceAccounts(namespace).Get(context.TODO(), saName, metav1.GetOptions{})
+	if err == nil { // Service account exists, update it
+		sa.ObjectMeta.Name = saName
+		sa.ObjectMeta.Namespace = namespace
+		_, err = client.CoreV1().ServiceAccounts(namespace).Update(context.TODO(), sa, metav1.UpdateOptions{})
+		if err != nil {
+			return fmt.Errorf("failed to update service account: %w", err)
+		}
+	} else { // it does not exist, create it
+		sa = &corev1.ServiceAccount{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      saName,
+				Namespace: namespace,
+			},
+		}
+		_, err = client.CoreV1().ServiceAccounts(namespace).Create(context.TODO(), sa, metav1.CreateOptions{})
+		if err != nil {
+			return fmt.Errorf("failed to create service account: %w", err)
+		}
+	}
+
+	// Create roleBinding to bind the given roles the service account
+	err = roleBinding(client, namespace, saName, config.Launcher.Data.RolesBinding)
+	if err != nil {
+		return fmt.Errorf("failed to create roles and bind to service account: %w", err)
 	}
 
 	return nil
