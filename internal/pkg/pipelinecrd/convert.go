3ee5650a517070358d11fe0976d2ee05a2c3a6d5
Sergio Tejón <sergio.tejon@gmail.com>
2024-10-27 12:07:00 +0100

    feat(internal/pkg/pipelinecrd): Create convert package for CRD conversion
    - Initial implementation of the convert package for managing Custom Resource Definitions (CRDs)

---
+++ internal/pkg/pipelinecrd/convert.go
@@ -0,0 +1,25 @@
+package pipelinecrd
+
+import (
+	"encoding/json"
+)
+
+// ConvertToPipelines converts the raw data to a PipelineSpec struct
+// This function convert only one raw pipeline to a PipelineSpec struct
+// This would be the future version for normalize.go when it's refactored
+func ConvertToPipelines(data interface{}) (PipelineSpec, error) {
+	// Convert each item to YAML
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return PipelineSpec{}, err
+	}
+
+	// Unmarshal YAML to PipelineSpec struct
+	var pipeline PipelineSpec
+	err = json.Unmarshal(jsonData, &pipeline)
+	if err != nil {
+		return PipelineSpec{}, err
+	}
+
+	return pipeline, nil
+}
