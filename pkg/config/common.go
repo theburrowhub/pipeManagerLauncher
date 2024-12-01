1e79079f4326b84b8fca71928b322046a1417e07
Sergio Tejón <sergio.tejon@gmail.com>
2024-12-02 00:25:01 +0100

    refactor(pkg/config): Convert YAML to JSON for configuration data structures
    - Loading functions in 'launcher.go' and 'common.go'
    - Update YAML to JSON conversion in loadConfigFromFile() and LoadLauncherConfig() functions
    - Modify LogConfig, CommonStruct, LauncherStruct, BucketConfig and BucketCredentials types in both files to support JSON marshaling

--- pkg/config/common.go
+++ pkg/config/common.go
@@ -4,19 +4,19 @@ package config
 // LogConfig defines the logging configuration.
 // It captures the logging level, file, and format.
 type LogConfig struct {
-	Level  string `yaml:"level"`  // Level is the logging level (e.g., "info")
-	File   string `yaml:"file"`   // File is the file to write the logs to
-	Format string `yaml:"format"` // Format is the log format (e.g., "json")
+	Level  string `json:"level"`  // Level is the logging level (e.g., "info")
+	File   string `json:"file"`   // File is the file to write the logs to
+	Format string `json:"format"` // Format is the log format (e.g., "json")
 }
 
 // CommonStruct defines the common configuration.
 type CommonStruct struct {
-	Log LogConfig `yaml:"log"` // Log is the logging configuration
+	Log LogConfig `json:"log"` // Log is the logging configuration
 }
 
 // CommonConfig defines the common configuration.
 type CommonConfig struct {
-	Data CommonStruct `yaml:"common"` // Data is the common configuration data
+	Data CommonStruct `json:"common"` // Data is the common configuration data
 }
 
 var Common CommonConfig // Common is the common configuration for all components
--- pkg/config/launcher.go
+++ pkg/config/launcher.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"fmt"
-
 	corev1 "k8s.io/api/core/v1"
 
 	"github.com/sergiotejon/pipeManagerLauncher/internal/pkg/version"
@@ -11,27 +10,27 @@ import (
 // LauncherStruct defines the launcher configuration.
 // It captures the image name, pull policy, tag, namespace, job name prefix, and timeout.
 type LauncherStruct struct {
-	ImageName       string       `yaml:"imageName"`       // ImageName is the name of the Docker image to be used
-	PullPolicy      string       `yaml:"pullPolicy"`      // PullPolicy is the policy to use when pulling the image
-	Tag             string       `yaml:"tag"`             // Tag is the tag of the Docker image to be used
-	Namespace       string       `yaml:"namespace"`       // Namespace is the Kubernetes namespace to deploy the job
-	JobNamePrefix   string       `yaml:"jobNamePrefix"`   // JobNamePrefix is the prefix to use for the job name
-	Timeout         int64        `yaml:"timeout"`         // Timeout is the maximum time in seconds to wait for the job to complete
-	BackoffLimit    int32        `yaml:"backoffLimit"`    // BackoffLimit is the number of retries before considering the job as failed
-	ConfigmapName   string       `yaml:"configmapName"`   // ConfigmapName is the name of the ConfigMap to use
-	CloneDepth      int          `yaml:"cloneDepth"`      // CloneDepth is the depth to use when cloning the Git repository
-	RolesBinding    []string     `yaml:"rolesBinding"`    // RolesBinding is the list of roles to bind to the Service Account
-	ArtifactsBucket BucketConfig `yaml:"artifactsBucket"` // ArtifactsBucket is the bucket configuration for storing the artifacts
-	PipelineType    string       `yaml:"pipelineType"`    // PipelineType is the type of pipeline to use (e.g., "tekton")
+	ImageName       string       `json:"imageName"`       // ImageName is the name of the Docker image to be used
+	PullPolicy      string       `json:"pullPolicy"`      // PullPolicy is the policy to use when pulling the image
+	Tag             string       `json:"tag"`             // Tag is the tag of the Docker image to be used
+	Namespace       string       `json:"namespace"`       // Namespace is the Kubernetes namespace to deploy the job
+	JobNamePrefix   string       `json:"jobNamePrefix"`   // JobNamePrefix is the prefix to use for the job name
+	Timeout         int64        `json:"timeout"`         // Timeout is the maximum time in seconds to wait for the job to complete
+	BackoffLimit    int32        `json:"backoffLimit"`    // BackoffLimit is the number of retries before considering the job as failed
+	ConfigmapName   string       `json:"configmapName"`   // ConfigmapName is the name of the ConfigMap to use
+	CloneDepth      int          `json:"cloneDepth"`      // CloneDepth is the depth to use when cloning the Git repository
+	RolesBinding    []string     `json:"rolesBinding"`    // RolesBinding is the list of roles to bind to the Service Account
+	ArtifactsBucket BucketConfig `json:"artifactsBucket"` // ArtifactsBucket is the bucket configuration for storing the artifacts
+	PipelineType    string       `json:"pipelineType"`    // PipelineType is the type of pipeline to use (e.g., "tekton")
 }
 
 // BucketConfig defines the bucket configuration.
 type BucketConfig struct {
-	URL         string            `yaml:"url"`                   // URL is the URL of the bucket
-	BasePath    string            `yaml:"basePath"`              // BasePath is the name of the bucket
-	SecretName  string            `yaml:"secretName"`            // SecretName is the name of the secret to use when accessing the bucket
-	Parameters  map[string]string `yaml:"parameters,omitempty"`  // Parameters is a map of additional parameters for the bucket
-	Credentials BucketCredentials `yaml:"credentials,omitempty"` // Credentials is the credentials to use when accessing the bucket
+	URL         string            `json:"url"`                   // URL is the URL of the bucket
+	BasePath    string            `json:"basePath"`              // BasePath is the name of the bucket
+	SecretName  string            `json:"secretName"`            // SecretName is the name of the secret to use when accessing the bucket
+	Parameters  map[string]string `json:"parameters,omitempty"`  // Parameters is a map of additional parameters for the bucket
+	Credentials BucketCredentials `json:"credentials,omitempty"` // Credentials is the credentials to use when accessing the bucket
 }
 
 // BucketCredentials defines the bucket credentials.
@@ -39,15 +38,15 @@ type BucketConfig struct {
 // Usually, the credentials are stored in a secret and mounted as a volume or as environment variables.
 // Mutually exclusive, but both can be used.
 type BucketCredentials struct {
-	Env          []corev1.EnvVar      `yaml:"env,omitempty"`          // Env is the environment variables to use
-	Volumes      []corev1.Volume      `yaml:"volumes,omitempty"`      // Volumes is the volumes to use
-	VolumeMounts []corev1.VolumeMount `yaml:"volumeMounts,omitempty"` // VolumeMounts is the volume mounts to use
+	Env          []corev1.EnvVar      `json:"env,omitempty"`          // Env is the environment variables to use
+	Volumes      []corev1.Volume      `json:"volumes,omitempty"`      // Volumes is the volumes to use
+	VolumeMounts []corev1.VolumeMount `json:"volumeMounts,omitempty"` // VolumeMounts is the volume mounts to use
 }
 
 // LauncherConfig defines the launcher configuration.
 // It captures the launcher configuration data from the root of the configuration file.
 type LauncherConfig struct {
-	Data LauncherStruct `yaml:"launcher"`
+	Data LauncherStruct `json:"launcher"`
 }
 
 var Launcher LauncherConfig // Launcher is the global launcher configuration
